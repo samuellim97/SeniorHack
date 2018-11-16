3cdba078ae86340e74f36f0bb02ebe05f2d411bf
yin3797 <yin3797@gmail.com>
2018-11-16 20:46:19 +0800

    changed stars

--- UserReview.php
+++ UserReview.php
@@ -13,6 +13,7 @@ $username = $_SESSION['username'];
 <head>
   <title>User Reviews</title>
   <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/SeniorHack.css">
   <link rel="stylesheet" href="css/userReview.css">
@@ -20,26 +21,22 @@ $username = $_SESSION['username'];
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 
 <style>
 
 body{background-color:#f0f3f5;}
-
-
 .nav-pills > li.active > a, .nav-pills > li.active > a:hover, .nav-pills > li.active > a:focus {
     color:white;
     background-color:#0B7A75;}
-
 .nav_style {
 	background-color:#0B7A75;
 	border:none;}
-
 .topnav li a{
 	text-align:center;
 	color:white !important;
 	padding-right: 35px;
 	padding-left: 35px;}
-
 @media (min-width: 768px) {
   .navbar-nav.navbar-center {
     position: absolute;
@@ -47,11 +44,48 @@ body{background-color:#f0f3f5;}
     transform: translatex(-50%);
   }
 }
+#btn-logout {
+ border-color:white ;
+ background:#0B7A75;
+ color:white;
+}
 #banner {
 	background:url(img/signup-banner.png) no-repeat top center fixed;
 	background-size:cover;
 	height:500px;
 	width:100%;}
+
+  .font {font-size:17px;}
+  .page-footer{
+  background-size:cover;
+  	width:100%;
+  	text-align:center;
+  	background-color:#d7c9aa;}
+  	#defaultOpen{background-color:}
+
+div.stars {
+  width: 270px;
+  display: inline-block;}
+input.star { display: none; }
+label.star {
+    float: right;
+    padding: 10px;
+    font-size: 36px;
+    color: #444;
+    transition: all .2s;}
+input.star:checked ~ label.star:before {
+  content: '\f005';
+  color: #FD4;
+  transition: all .25s;}
+input.star-5:checked ~ label.star:before {
+  color: #FE7;
+  text-shadow: 0 0 20px #952;}
+input.star-1:checked ~ label.star:before { color: #F62; }
+label.star:hover { transform: rotate(-15deg) scale(1.3); }
+label.star:before {
+  content: '\f006';
+  font-family: FontAwesome;}
+
 
 
 </style>
@@ -73,11 +107,10 @@ body{background-color:#f0f3f5;}
         <li><image style="height:50px; padding-left: 10%" src="img/logo.png"></image></li>
     </ul>
     <ul class="nav navbar-nav navbar-center topnav">
-        <li><a href="#"></a></li>
         <li><a href="#">Service Providers</a></li>
 		<li><a href="#" id="defaultSelected"> Requests</a></li>
 		<li><a href="#">Contact Us</a></li>
-		<li><a href="#">Help</a></li>
+		<li><a href="helpCentre.html">Help</a></li>
     </ul>
 	<!--Log Out-->
    <div class="collapse navbar-collapse" id="cl-mainNavbar">
@@ -118,157 +151,178 @@ of service providers which will bring about convenience and comfort in their liv
       </div>
     </div>
     <div>
-        <form action="#">
-          <div class="form-group" style="margin-top: 3%">
-            <label for="rating"></label>
-                  <fieldset class="rate">Rate:
-                    <input type="radio" id="rating10" name="rating" value="10" /><label for="rating10" title="5 stars"></label>
-                    <input type="radio" id="rating9" name="rating" value="9" /><label class="half" for="rating9" title="4 1/2 stars"></label>
-                    <input type="radio" id="rating8" name="rating" value="8" /><label for="rating8" title="4 stars"></label>
-                    <input type="radio" id="rating7" name="rating" value="7" /><label class="half" for="rating7" title="3 1/2 stars"></label>
-                    <input type="radio" id="rating6" name="rating" value="6" /><label for="rating6" title="3 stars"></label>
-                    <input type="radio" id="rating5" name="rating" value="5" /><label class="half" for="rating5" title="2 1/2 stars"></label>
-                    <input type="radio" id="rating4" name="rating" value="4" /><label for="rating4" title="2 stars"></label>
-                    <input type="radio" id="rating3" name="rating" value="3" /><label class="half" for="rating3" title="1 1/2 stars"></label>
-                    <input type="radio" id="rating2" name="rating" value="2" /><label for="rating2" title="1 star"></label>
-                    <input type="radio" id="rating1" name="rating" value="1" /><label class="half" for="rating1" title="1/2 star"></label>
-                    <input type="radio" id="rating0" name="rating" value="0" /><label for="rating0" title="No star"></label>
-                  </fieldset>
-              </div>
-              <div class="form-group">
-                <label for="comment"></label>
-                  <input type="text" class="form-control" id="comment" placeholder="What do you think?">
-
-              </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+    <form action="#">
+    <div class="stars">
+    <div class="form-group" style="margin-top: 5%">
+      Rate:
+      <fieldset class="rate">
+        <span style="margin-top:10px"/>
+        <input class="star star-5" id="star-5" type="radio" name="star"/>
+        <label class="star star-5" for="star-5"></label>
+        <input class="star star-4" id="star-4" type="radio" name="star"/>
+        <label class="star star-4" for="star-4"></label>
+        <input class="star star-3" id="star-3" type="radio" name="star"/>
+        <label class="star star-3" for="star-3"></label>
+        <input class="star star-2" id="star-2" type="radio" name="star"/>
+        <label class="star star-2" for="star-2"></label>
+        <input class="star star-1" id="star-1" type="radio" name="star"/>
+        <label class="star star-1" for="star-1"></label>
+      </fieldset>
+    </div>
+    </div>
+    <div class="form-group">
+        <label for="comment"></label>
+            <input type="text" class="form-control" id="comment" placeholder="What do you think?">
+    </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
     </div>
     </div>
 <br>
 <br>
 <br>
-<div class="container">
-<div id="user_reviews2" class="row">
-  <div id="ratings" class="col-lg-2  col-sm-2">
-          <h5>Service Provider Ratings</h5>
-          <h1>3.8 / 5</h1>
-          <span style="font-size: 11px">based on 38 user reviews</span>
-  </div>
-  <div class="col-lg-8  col-sm-10" style="height:500px; overflow:auto; margin-top:-3%">
-    <br>
-    <br>
-  <table id="reviewsTable" class="tb1">
-    <tr>
-      <td>
-      <p style="margin:10px 15px; float:left;">21/7/2018, Renee Lim
-      <span style="float:right;">3 stars</span>
-      <br><br>Thank you for your professional follow up and follow through on
-      services rendered at my home. I highly recommend your services to
-      friends and family.
-      </p>
-      </td>
-    </tr>
-    <tr>
-      <td>
-      <p style="margin:10px 15px; float:left;">21/7/2018, Renee Lim
-      <span style="float:right;">3 stars</span>
-      <br><br>Thank you for your professional follow up and follow through on
-      services rendered at my home. I highly recommend your services to
-      friends and family.
-      </p>
-      </td>
-    </tr>
-    <tr>
-      <td>
-      <p style="margin:10px 15px; float:left;">21/7/2018, Renee Lim
-      <span style="float:right;">3 stars</span>
-      <br><br>Thank you for your professional follow up and follow through on
-      services rendered at my home. I highly recommend your services to
-      friends and family.
-      </p>
-      </td>
-    </tr>
-    <tr>
-      <td>
-      <p style="margin:10px 15px; float:left;">21/7/2018, Renee Lim
-      <span style="float:right;">3 stars</span>
-      <br><br>Thank you for your professional follow up and follow through on
-      services rendered at my home. I highly recommend your services to
-      friends and family.
-      </p>
-      </td>
-    </tr>
-    <tr>
-      <td>
-      <p style="margin:10px 15px; float:left;">21/7/2018, Renee Lim
-      <span style="float:right;">3 stars</span>
-      <br><br>Thank you for your professional follow up and follow through on
-      services rendered at my home. I highly recommend your services to
-      friends and family.
-      </p>
-      </td>
-    </tr>
-    <tr>
-      <td>
-      <p style="margin:10px 15px; float:left;">21/7/2018, Renee Lim
-      <span style="float:right;">3 stars</span>
-      <br><br>Thank you for your professional follow up and follow through on
-      services rendered at my home. I highly recommend your services to
-      friends and family.
-      </p>
-      </td>
-    </tr>
-    <tr>
-      <td>
-      <p style="margin:10px 15px; float:left;">21/7/2018, Renee Lim
-      <span style="float:right;">3 stars</span>
-      <br><br>Thank you for your professional follow up and follow through on
-      services rendered at my home. I highly recommend your services to
-      friends and family.
-      </p>
-      </td>
-    </tr>
-    <tr>
-      <td>
-      <p style="margin:10px 15px; float:left;">21/7/2018, Renee Lim
-      <span style="float:right;">3 stars</span>
-      <br><br>Thank you for your professional follow up and follow through on
-      services rendered at my home. I highly recommend your services to
-      friends and family.
-      </p>
-      </td>
-    </tr>
-  </table>
-</div>
-	</div>
-</div>
 </div>
 <!--End of View User Reviews-->
 
-<footer id="footer-bg" class="footer" style="width:100%">
-<div class="container">
+<!--Testing template-->
+<div class="container" style="margin-left: 6%">
+
+		<div class="row">
+			<div class="col-sm-3">
+				<div class="rating-block">
+					<h4>Average user rating</h4>
+					<h2 class="bold padding-bottom-7">4.3 <small>/ 5</small></h2>
+					<button type="button" class="btn btn-warning btn-sm" aria-label="Left Align">
+					  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+					</button>
+					<button type="button" class="btn btn-warning btn-sm" aria-label="Left Align">
+					  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+					</button>
+					<button type="button" class="btn btn-warning btn-sm" aria-label="Left Align">
+					  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+					</button>
+					<button type="button" class="btn btn-default btn-grey btn-sm" aria-label="Left Align">
+					  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+					</button>
+					<button type="button" class="btn btn-default btn-grey btn-sm" aria-label="Left Align">
+					  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+		</div>
+
+		<div class="row" style="margin-bottom:5%">
+			<div class="col-sm-7">
+				<hr/>
+				<div class="review-block">
+					<div class="row">
+						<div class="col-sm-3">
+							<div class="review-block-name"><a href="#">nktailor</a></div>
+							<div class="review-block-date">January 29, 2016</div>
+						</div>
+						<div class="col-sm-9">
+							<div class="review-block-rate">
+								<button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+								  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+								</button>
+								<button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+								  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+								</button>
+								<button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+								  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+								</button>
+								<button type="button" class="btn btn-default btn-grey btn-xs" aria-label="Left Align">
+								  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+								</button>
+								<button type="button" class="btn btn-default btn-grey btn-xs" aria-label="Left Align">
+								  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+								</button>
+							</div>
+							<div class="review-block-title">this was nice in buy</div>
+							<div class="review-block-description">this was nice in buy. this was nice in buy. this was nice in buy. this was nice in buy this was nice in buy this was nice in buy this was nice in buy this was nice in buy</div>
+						</div>
+					</div>
+					<hr/>
+					<div class="row">
+						<div class="col-sm-3">
+							<div class="review-block-name"><a href="#">nktailor</a></div>
+							<div class="review-block-date">January 29, 2016</div>
+						</div>
+						<div class="col-sm-9">
+							<div class="review-block-rate">
+								<button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+								  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+								</button>
+								<button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+								  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+								</button>
+								<button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+								  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+								</button>
+								<button type="button" class="btn btn-default btn-grey btn-xs" aria-label="Left Align">
+								  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+								</button>
+								<button type="button" class="btn btn-default btn-grey btn-xs" aria-label="Left Align">
+								  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+								</button>
+							</div>
+							<div class="review-block-title">this was nice in buy</div>
+							<div class="review-block-description">this was nice in buy. this was nice in buy. this was nice in buy. this was nice in buy this was nice in buy this was nice in buy this was nice in buy this was nice in buy</div>
+						</div>
+					</div>
+					<hr/>
+					<div class="row">
+						<div class="col-sm-3">
+							<div class="review-block-name"><a href="#">nktailor</a></div>
+							<div class="review-block-date">January 29, 2016</div>
+						</div>
+						<div class="col-sm-9">
+							<div class="review-block-rate">
+								<button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+								  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+								</button>
+								<button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+								  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+								</button>
+								<button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+								  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+								</button>
+								<button type="button" class="btn btn-default btn-grey btn-xs" aria-label="Left Align">
+								  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+								</button>
+								<button type="button" class="btn btn-default btn-grey btn-xs" aria-label="Left Align">
+								  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+								</button>
+							</div>
+							<div class="review-block-title">this was nice in buy</div>
+							<div class="review-block-description">this was nice in buy. this was nice in buy. this was nice in buy. this was nice in buy this was nice in buy this was nice in buy this was nice in buy this was nice in buy</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+    </div> <!-- /container -->
+
+<!--End of testing template-->
+
+<!-- Footer -->
+<footer class="page-footer" style="color:black">
 <div class="row">
 
-<!--Contact No-->
-  <div class="col-lg-4 col-lg-offset-0 col-md-4 col-md-offset-0 col-sm-4 col-sm-offset-0 col-xs-6 col-xs-offset-4">
-      <p class="footer-left" style="text-indent:10px">Contact Us At <br><span style="font-size:18px;font-weight:bold;"> 1300-88-2525</span></p>
-      </div>
-
-    <div class="col-lg-4 col-lg-offset-0 col-md-4 col-md-offset-0 col-sm-4 col-sm-offset-0 col-xs-10 col-xs-offset-1 ">
-      <p class="footer_mid">&copy;Copyright SeniorHack 2018 <br>All rights reserved</p>
-    </div>
-
-<!--Links to Social Media-->
-    <div class="col-lg-4 col-lg-offset-0 col-md-4 col-md-offset-0 col-sm-4 col-sm-offset-0 col-xs-6 col-xs-offset-1 ">
-    <ul class="social-network social-circle footer-right ">
-	<a style="color:black">Join Us At </a> <br>
-       <a href="https://facebook.com/"><i class="fa fa-facebook-square" style="font-size:2em;color:black;margin-right:18%"></i></a>
-		<a href="https://twitter.com/"><i class="fa fa-twitter" style="font-size:2em;color:black"></i></a>
-     </ul>
+  <!-- Copyright -->
+  <div class="col-lg-4 col-md-12"  style="margin-top:1%;margin-bottom:1%">Contact Us At <br><span style="font-size:18px;font-weight:bold;"> 1300-88-2525</span>
   </div>
-</div>
+  <div class="col-lg-4 col-md-12" style="margin-top:1%;margin-bottom:1%">&copy;Copyright SeniorHack 2018 <br>All rights reserved
+  </div>
+  <div class="col-lg-4 col-md-12 " style="margin-top:1%;margin-bottom:1%"><a>Join Us At </a> <br>
+       <a href="https://facebook.com/"><i class="fa fa-facebook-square" style="font-size:1.5em;color:black;margin-right:5%"></i></a>
+		<a href="https://twitter.com/"><i class="fa fa-twitter" style="font-size:1.5em;color:black"></i></a>
+  </div>
+  <!-- Copyright -->
 </div>
 </footer>
+  <!-- Footer -->
 
 <!--Request anchor and New Request Tab are selected by default-->
 <script>
@@ -279,20 +333,11 @@ $('.button').click(function() {
     $(this).addClass('active');
 })
 
-
-jQuery(document).ready(function($) {
-    $(".clickable-row").click(function() {
-        window.location = $(this).data("href");
-    });
-})
-$("#edit").on("click", function(){
-    $("#myModal").modal("hide");
-    $("#myModal").on("hidden.bs.modal",function(){
-    $("#detailsModal").modal("show");
-    });
-});
-
-
 </script>
+
+<!-- Bootstrap core JavaScript -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </body>
 </html>
