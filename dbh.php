7cba3474a2ef035931cfdbef898e0a9ce21371d0
yin3797 <yin3797@gmail.com>
2018-11-22 19:40:06 +0800

    Update dbh.php

--- dbh.php
+++ dbh.php
@@ -1,13 +1,4 @@
 <?php
-<<<<<<< HEAD
-session_start();
- $servername = "localhost";
- $username = "root";
- $password = "";
- $dbname = "SeniorHack";
- $con = new mysqli($servername, $username, $password, $dbname);
- ?>
-=======
 $dbservername = "localhost";
 $dbusername = "root";
 $dbpassword = "";
@@ -18,4 +9,3 @@ if ($con->connect_error){
   die("Connection failed: " . $con->connect_error);
 }
 ?>
->>>>>>> 9ed8700c78fd2141252a9aebac64778d75ceb149
